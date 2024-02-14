4f3c502df42cd0d2ba4bc73483c6b589729c451a
Muhammad Alam <malam53345@gmail.com>
2024-02-14 20:38:52 +0500

    logos section added

---
+++ app/components/home/LogoSection.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React from "react";
+
+const LogoSection = () => {
+  return (
+    <div className="py-[2rem] grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-[1.5rem] place-items-center px-[20px]">
+      <Image src="/images/11.png" width={50} height={50} alt="" />
+      <Image src="/images/21.svg" width={100} height={50} alt="" />
+      <Image src="/images/31.png" width={100} height={100} alt="" />
+      <Image src="/images/41.png" width={170} height={100} alt="" />
+      <Image src="/images/51.png" width={35} height={30} alt="" />
+      <Image
+        src="/images/12.png"
+        width={150}
+        height={80}
+        alt=""
+        className="hidden xs:flex"
+      />
+      <Image
+        src="/images/22.svg"
+        width={70}
+        height={70}
+        alt=""
+        className="hidden sm:flex"
+      />
+      <Image
+        src="/images/32.png"
+        width={200}
+        height={100}
+        alt=""
+        className="hidden md:flex"
+      />
+      <Image
+        src="/images/42.png"
+        width={50}
+        height={50}
+        alt=""
+        className="hidden md:flex"
+      />
+      <Image
+        src="/images/52.png"
+        width={170}
+        height={100}
+        alt=""
+        className="hidden md:flex"
+      />
+    </div>
+  );
+};
+
+export default LogoSection;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import HeroSection from "./components/home/HeroSection";
+import LogoSection from "./components/home/LogoSection";
 
 export default function Home() {
   return (
-    <div className="container opacity-90">
+    <div className="container">
       <HeroSection />
+      <LogoSection />
     </div>
   );
 }
